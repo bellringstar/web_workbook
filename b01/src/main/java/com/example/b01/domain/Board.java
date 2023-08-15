268b949f518468e49b757233beddd3ce6182f9f5
bellring <starringbell@gmail.com>
2023-08-15 20:15:34 +0900

    querydsl

---
+++ b01/src/main/java/com/example/b01/domain/Board.java
@@ -0,0 +1,32 @@
+package com.example.b01.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Board extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    public void change(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
---
+++ b01/src/main/java/com/example/b01/repository/search/BoardSearchImpl.java
@@ -0,0 +1,91 @@
+package com.example.b01.repository.search;
+
+import com.example.b01.domain.Board;
+import com.example.b01.domain.QBoard;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.JPQLQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardSearch {
+
+    //QuerydslRepositorySupport를 상속받아 새로운 클래스를 정의할 때 해당 클래스의 생성자에서 super 키워드를 사용하여 상위 클래스의 생성자를 호출해야한다.
+    //이렇게 함으로써 하위 클래스는 상위 클래스에서 정의도니 필드와 메서드들을 초기화하고 사용할 수 있다.
+    public BoardSearchImpl(){
+        super(Board.class);
+    }
+
+    @Override
+    public Page<Board> searchAll(String[] types, String keyword, Pageable pageable) {
+
+        QBoard board = QBoard.board;
+        JPQLQuery<Board> query = from(board);
+
+        if ( (types != null && types.length > 0) && keyword != null) {
+            //검색 조건과 키워드가 있다면
+
+            BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+            for (String type : types) {
+
+                switch (type) {
+                    case "t" -> {
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    }
+                    case "c" -> {
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                    }
+                    case "w" -> {
+                        booleanBuilder.or(board.writer.contains(keyword));
+                    }
+                }
+            }
+            query.where(booleanBuilder);
+        }
+        // bno > 0
+        query.where(board.bno.gt(0L));
+        //paging
+        this.getQuerydsl().applyPagination(pageable, query);
+
+        List<Board> list = query.fetch();
+
+        long count = query.fetchCount();
+
+//        return null;
+        return new PageImpl<>(list, pageable, count);
+    }
+
+
+    @Override
+    public Page<Board> search1(Pageable pageable) {
+
+        QBoard board = QBoard.board; //Q도메인 객체
+
+        JPQLQuery<Board> query = from(board); //select.. from board
+
+        query.where(board.title.contains("1")); //where title like...
+
+        //paging
+        this.getQuerydsl().applyPagination(pageable, query);
+
+        List<Board> list = query.fetch();
+
+        long count = query.fetchCount();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        booleanBuilder.or(board.title.contains("11")); // title like ...
+        booleanBuilder.or(board.content.contains("11")); // content like ...
+
+        query.where(booleanBuilder);
+        query.where(board.bno.gt(0L));
+
+        return null;
+    }
+}
